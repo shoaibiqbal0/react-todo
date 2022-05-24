b8e48a1b0c145207933df944831445211d4d4398
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-05-24 21:45:08 +0100

    Added Dark Mode toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { VStack, IconButton, Heading } from "@chakra-ui/react";
+import { VStack, IconButton, Heading, useColorMode } from "@chakra-ui/react";
 import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 function App() {
   const [todos, setTodos] = useState(["Buy Milk", "Buy Bread", "Buy Wheat"]);
+  const { colorMode, toggleColorMode } = useColorMode();
 
   function addTodo(todo) {
     setTodos((previousValue) => {
@@ -24,10 +25,11 @@ function App() {
     <VStack p="4">
       <IconButton
         aria-label="Switch Mode"
-        icon={<FaSun />}
+        icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
         isRound={true}
         size="lg"
         alignSelf="flex-end"
+        onClick={toggleColorMode}
       />
       <Heading
         pb="10"
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 const rootElement = document.querySelector("#root");
 const root = createRoot(rootElement);
@@ -10,6 +10,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode="light" />
       <App />
     </ChakraProvider>
   </StrictMode>
