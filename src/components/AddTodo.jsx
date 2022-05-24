6a9de427f15e38fb45b9d45ff67a32823ff61878
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-05-24 21:08:01 +0100

    Removed some redundant code

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,7 +1,7 @@
 import { HStack, VStack } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { useState } from "react";
 
 function AddTodo(props) {
   const [todo, setTodo] = useState("");
