34fa434253bbe493a910e678da1548d7595c5d08
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-05-26 21:42:14 +0100

    Added get and post routes

--- backend/api.js
+++ backend/api.js
@@ -19,8 +19,29 @@ const toDoList = mongoose.model("toDoList", toDoListSchema);
 
 // Get the homepage
 app.get("/", (req, res) => {
-  console.log("It's working!");
+  toDoList.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
+
+app.post("/", (req, res) => {
+  const newListItem = new toDoList({
+    content: req.body.content,
+  });
+  newListItem.save((err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("Data was saved successfully.");
+    }
+  });
+});
+
+app.delete("/", (req, res) => {});
 
 // Start server on port 5000
 app.listen(5000, () => {
